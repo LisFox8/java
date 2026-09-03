ad47fa99da5d9f441bf563dc47e93a00c26349d6
LisFox8 <bartoszrutkowski359@gmail.com>
2026-09-03 10:58:27 +0200

    Add files via upload

---
+++ java/zewnętrzny-rutkowski.js
@@ -0,0 +1,5 @@
+document.write("mam numer w dzienniku 17"+"<br>");
+document.write("pierwszy wiersz to: 14  tak tak 7"+"<br>");
+document.write("Drugi wiersz to:    14  tak tak 5"+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("blue").bold().strike().fontsize(7)+"<br>"); 
+document.write(document.lastModified.fontcolor("blue").bold().strike().fontsize(5)); 
